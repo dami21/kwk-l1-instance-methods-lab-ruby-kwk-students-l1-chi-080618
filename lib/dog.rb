1e0e74475f7d70b23e19e2783aba752f74cc1879
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-10 16:28:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,3 +2,15 @@
 class Dog 
   def bark 
     puts "Woof!"
+  end 
+  def sit 
+    puts "the Dog is sitting."
+  end 
+  def Person
+  end 
+  def talk 
+    puts "Hello World"
+  end 
+  def walk
+    puts "The Person is walking"
+end 
